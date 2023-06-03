aa801deee9136167f2cc346d6c8bed05be01b075
chandra shekhar <chandra827009@gmail.com>
2023-06-03 13:57:10 +0530

    first commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Style from "../Styles/navbar.module.css";
+import MenuBarForMobile from "./MenuBarForMObile";
+
+const Navbar = () => {
+  return (
+    <div className={Style.navbar}>
+      <div className={Style.menuIcon}>
+        {/* <i className="fa-solid fa-bars"></i> */}
+        <MenuBarForMobile />
+      </div>
+      <div className={Style.navbarLogo}>
+        <img
+          src="https://user-images.githubusercontent.com/99132893/232487978-470f10f8-1077-4fc2-b691-c9ed4859dd21.jpg"
+          alt="web-logo"
+        />&nbsp;
+        <span>Guruji</span>
+      </div>
+      <div className={Style.menu}>
+        <p>Home</p>
+        <p>Call with Astrologer</p>
+        <p>Live(Comming Soon)</p>
+      </div>
+      <div className={Style.user}>
+        <img
+          src="https://avatars.githubusercontent.com/u/100562557?s=400&u=ba1fe6d741335a8deaa3cfcec6187fbc9738500e&v=4"
+          alt="user-profile-pic"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
+
